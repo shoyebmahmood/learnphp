03f8b0e60e254a47e0ed06e1fca3b5299728e3fb
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-13 20:53:09 +0600

    variable constant and comments in php

---
+++ variable.php
@@ -0,0 +1,24 @@
+<?php
+//Variable 
+//single cote means string and without cote means number;
+//variable must be alfaneumeric, and start must be alphabet.
+/*
+first $word means age; then php convert age to variable $age which is 16.
+*/
+$age = 16;
+$word = 'age';
+echo $$word;
+echo PHP_EOL;
+//in single cote if we place string variable; php does not recognize it as a variable;
+//. or concatation and good practice is to keep variable is curley braces
+$name = "Bangladesh";
+$question = "How are you?";
+echo "hello {$name}, {$question}";
+echo PHP_EOL;
+/*
+Constant 
+*/
+define('PI',3.14);
+echo constant("PI");
+echo "\n";
+echo PI;
