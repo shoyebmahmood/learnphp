0b03246131f566ab6427518e5f0bca6d28669794
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-13 22:10:00 +0600

    change

--- arithmaticoperator.php
+++ arithmaticoperator.php
@@ -9,6 +9,6 @@ echo PHP_EOL;
 $number = (10-7) *8;//BODMAS(Bracket Or Division Multification, ADDITION and Subtraction)
 echo $number;
 $number++//$number = $number + 1,
-$number--//$number = $number - 1,
+$number--//$number = $number -1,
 
 
