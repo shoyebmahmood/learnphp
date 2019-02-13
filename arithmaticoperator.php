bd9828a52cc7b9ef9223af9280bf780525dc16c4
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-13 22:08:39 +0600

    +-*/%

--- arithmaticoperator.php
+++ arithmaticoperator.php
@@ -1,1 +1,14 @@
 <?php
+$number = 10;
+$number = $number + 12;
+echo $number;
+echo PHP_EOL;
+$number +=12;
+echo $number;
+echo PHP_EOL;
+$number = (10-7) *8;//BODMAS(Bracket Or Division Multification, ADDITION and Subtraction)
+echo $number;
+$number++//$number = $number + 1,
+$number--//$number = $number - 1,
+
+
