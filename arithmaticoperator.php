c23e8f43ca1a5b4d7094f994315f58ef9080b36c
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-02-14 09:35:28 +0600

    changes added

--- arithmaticoperator.php
+++ arithmaticoperator.php
@@ -8,7 +8,5 @@ echo $number;
 echo PHP_EOL;
 $number = (10-7) *8;//BODMAS(Bracket Or Division Multification, ADDITION and Subtraction)
 echo $number;
-$number++//$number = $number + 1,
-$number--//$number = $number -1,
-push
-
+echo PHP_EOL;
+$number++; // NUMBER=1+$NUMBER.
--- variable.php
+++ variable.php
@@ -1,6 +1,6 @@
 <?php
 //Variable 
-//single cote means string and without cote means number;
+//single quote means string and without quote means number;
 //variable must be alfaneumeric, and start must be alphabet.
 /*
 first $word means age; then php convert age to variable $age which is 16.
@@ -9,11 +9,11 @@ $age = 16;
 $word = 'age';
 echo $$word;
 echo PHP_EOL;
-//in single cote if we place string variable; php does not recognize it as a variable;
+//in single quote if we place string variable; php does not recognize it as a variable;
 //. or concatation and good practice is to keep variable is curley braces
 $name = "Bangladesh";
 $question = "How are you?";
-echo "hello {$name}, {$question}";
+echo "hello {$name}, {$question} and my age is {$age}  ";
 echo PHP_EOL;
 /*
 Constant 
