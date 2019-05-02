de33de3339819c6b530696d0669dd4cad3e24b26
shoyebmahmood <mahmoodshoyeb@gmail.com>
2019-05-02 17:22:38 +0600

    start array

---
+++ array.php
@@ -0,0 +1,15 @@
+<?php
+$n =12;
+$students = array(
+    "Rahim",
+    "Salam",
+    123,
+    "Fani",
+    456
+);
+echo $students[0];//here 0 or position is called offset!
+echo PHP_EOL;
+var_dump($students);
+echo PHP_EOL;
+//TO KNOW ELEMENT OF ARRAY
+echo count($students);
